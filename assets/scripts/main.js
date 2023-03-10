7851f128a7f6b29547290dc73cb68ec888b0ef70
Luka-Mlk <luka.milik1@gmail.com>
2023-03-10 12:24:27 +0100

    finished desktop site

---
+++ assets/scripts/main.js
@@ -0,0 +1,12 @@
+const scrollToBio = () => {
+  window.scrollTo(0, 0);
+};
+const scrollToSkill = () => {
+  window.scrollTo(0, document.body.scrollHeight / 6);
+};
+const scrollToWork = () => {
+  window.scrollTo(0, document.body.scrollHeight / 2.4);
+};
+const scrollToProject = () => {
+  window.scrollTo(0, document.body.scrollHeight / 1.4);
+};
